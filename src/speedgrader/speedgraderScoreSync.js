dec6140b2dc5576bc12b8b5035c548d9bd2dfaf2
JC <jccook@more.net>
2026-02-03 09:57:52 -0600

    polishing gradeSync UI

--- src/speedgrader/speedgraderScoreSync.js
+++ src/speedgrader/speedgraderScoreSync.js
@@ -534,9 +534,14 @@ async function createUIControls(courseId, assignmentId) {
             <option value="max" ${settings.method === 'max' ? 'selected' : ''}>MAX</option>
         </select>
         <span style="width: 1px; height: 1.75rem; background: rgb(209, 213, 219); margin: 0 0.25rem;"></span>
-        <span class="cg-score-chip" style="display: inline-flex; align-items: center; padding: 0 0.75rem; height: 2.25rem; background: #0b6a2b; color: #fff; font-weight: 700; border-radius: 0.35rem; white-space: nowrap;">
-            <span data-cg-assignment-score>--</span> pts
-        </span>
+        <div style="display: inline-flex; height: 3rem;">
+            <div style="display: flex; align-items: center; justify-content: center; padding: 0 0.75rem; height: 3rem; line-height: 3rem; background-color: rgb(245, 245, 245); border-radius: 0.35rem 0px 0px 0.35rem;">
+                <span style="white-space: nowrap;">Assignment Score</span>
+            </div>
+            <div style="display: flex; align-items: center; justify-content: center; padding: 0 0.75rem; height: 3rem; line-height: 3rem; background-color: rgb(3, 137, 61); border-radius: 0px 0.35rem 0.35rem 0px;">
+                <span style="color: #fff; font-weight: 700; white-space: nowrap;"><span data-cg-assignment-score>--</span> pts</span>
+            </div>
+        </div>
     `;
 
     const toggle = container.querySelector('[data-cg-toggle]');
