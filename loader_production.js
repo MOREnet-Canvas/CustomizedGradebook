6941451ae5397bb260684bd0a75a50ba6d109cc0
JC <jccook@more.net>
2026-02-04 11:00:47 -0600

    updating deployment scripting

--- loader_production.js
+++ loader_production.js
@@ -81,7 +81,7 @@
     // Read release configuration from managed block
     const release = (window.CG_MANAGED && window.CG_MANAGED.release) || {
         channel: "prod",
-        version: "v1.0.1",
+        version: "v1.0.2",
         source: "github_release"
     };
 
@@ -113,7 +113,7 @@ window.CG_MANAGED = window.CG_MANAGED || {};
 // Release configuration
 window.CG_MANAGED.release = {
     channel: "prod",
-    version: "v1.0.1",
+    version: "v1.0.2",
     source: "github_release"
 };
 
